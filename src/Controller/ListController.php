08f64b2b1902abf17629280a857b5a5e64468edd
DiegoAle2390 <diegoale2390@gmail.com>
2020-07-13 01:25:30 -0300

    Entidades, Get y Post Lists

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -2,29 +2,103 @@
 
 namespace App\Controller;
 
+use App\Entity\TaskList;
+use App\Repository\TaskListRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Request\ParamFetcher;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/api")
+ */
 class ListController extends AbstractFOSRestController
 {
-    /**
-     * @Rest\Get("/list", name="list_index")
-     */
-    public function index()
+    private $taskListRepository;
+    private $entityManager;
+
+    function __construct(TaskListRepository $taskListRepository, EntityManagerInterface $entityManager)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!'
-        ]);
+        $this->taskListRepository = $taskListRepository;
+        $this->entityManager = $entityManager;
     }
 
     /**
-     * @Rest\Get("/list/update", name="list_update")
+     * Mostrar Toda el listado
+     * @Rest\Get("/lists", name="get_lists")
      */
-    public function update()
+    public function getListsAction()
     {
-        return $this->json([
-            'message' => 'Function update'
-        ]);
+        $lists = $this->taskListRepository->findAll();
+
+        return $this->view($lists, Response::HTTP_OK);
+    }
+
+
+    /**
+     * Mostrar un registro del listado
+     * @Rest\Get("/lists/{id}", name="get_list")
+     */
+    public function getListAction($id)
+    {
+        $list = $this->taskListRepository->findOneBy(['id' => $id]);
+        return $this->view($list, Response::HTTP_OK);
+    }
+
+
+    /**
+     * Crear un nuevo registro
+     * @Rest\Post("/lists", name="post_lists")
+     * @Rest\RequestParam(name="title", description="title of the list", nullable=false)
+     * @param ParamFetcher $paramFetcher
+     * @return \FOS\RestBundle\View\View
+     */
+    public function postListsAction(ParamFetcher $paramFetcher)
+    {
+        $title = $paramFetcher->get('title');
+
+        if($title) {
+            $list = new TaskList();
+            $list->setTitle($title);
+
+            $this->entityManager->persist($list);
+            $this->entityManager->flush();
+
+            return $this->view($list, Response::HTTP_CREATED);
+        }
+
+        return $this->view(['title' => 'Error en el proceso'], Response::HTTP_BAD_REQUEST);
+    }
+
+
+    /**
+     * Ver las tareas del registro
+     * @Rest\Get("/api/lists/{id}/tasks", name="get_lists_tasks")
+     */
+    public function getListsTasksAction($id)
+    {
+
+    }
+
+
+    /**
+     * Actualizar el registro (por put)
+     * @Rest\Put("/api/lists", name="put_lists")
+     */
+    public function putListsAction()
+    {
+
+    }
+
+
+    /**
+     * Actualizar un registro (por patch)
+     * @Rest\Patch("/api/lists/{id}/background", name="background_lists")
+     */
+    public function patchListsAction($id)
+    {
+
     }
 }
---
+++ src/Entity/Note.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NoteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NoteRepository::class)
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Note
+{
+    use Timestamps;
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $note;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Task::class, inversedBy="notes")
+     */
+    private $task;
+
+    /*******************************************************************************************************************
+     * Metodos Metodos GET y SET de los atributos
+     ******************************************************************************************************************/
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(string $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getTask(): ?Task
+    {
+        return $this->task;
+    }
+
+    public function setTask(?Task $task): self
+    {
+        $this->task = $task;
+
+        return $this;
+    }
+
+    /*******************************************************************************************************************
+     * Metodos complementarios a la entidad
+     ******************************************************************************************************************/
+    function __toString()
+    {
+        return $this->getNote();
+    }
+}
