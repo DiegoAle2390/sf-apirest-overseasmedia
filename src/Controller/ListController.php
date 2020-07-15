975846091c3a3907efa4e76af80141a915368d8a
DiegoAle2390 <diegoale2390@gmail.com>
2020-07-15 07:28:16 -0300

    Actualizar el background y subir imagen al servidor

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -43,10 +43,12 @@ class ListController extends AbstractFOSRestController
     /**
      * Mostrar un registro del listado
      * @Rest\Get("/lists/{id}", name="get_list")
+     * @param TaskList $list
+     * @return \FOS\RestBundle\View\View
      */
-    public function getListAction($id)
+    public function getListAction(TaskList $list)
     {
-        $list = $this->taskListRepository->findOneBy(['id' => $id]);
+        //$list = $this->taskListRepository->findOneBy(['id' => $id]);
         return $this->view($list, Response::HTTP_OK);
     }
 
@@ -60,9 +62,11 @@ class ListController extends AbstractFOSRestController
      */
     public function postListsAction(ParamFetcher $paramFetcher)
     {
+        // TODO: Parametros provenientes de la petición
         $title = $paramFetcher->get('title');
 
-        if($title) {
+        // TODO: Se comprueba si no es null y se procede con la inserción
+        if (!is_null($title)) {
             $list = new TaskList();
             $list->setTitle($title);
 
@@ -72,49 +76,65 @@ class ListController extends AbstractFOSRestController
             return $this->view($list, Response::HTTP_CREATED);
         }
 
-        return $this->view(['title' => 'Error en el proceso'], Response::HTTP_BAD_REQUEST);
+        // TODO: En caso de fallar, devuelve un mensaje
+        return $this->view(['message' => 'Error en el proceso de inserción'], Response::HTTP_BAD_REQUEST);
     }
 
 
     /**
      * Ver las tareas del registro
      * @Rest\Get("/lists/{id}/tasks", name="get_lists_tasks")
+     * @param TaskList $list
+     * @return \FOS\RestBundle\View\View
      */
-    public function getListsTasksAction($id)
+    public function getListsTasksAction(TaskList $list)
     {
-
+        return $this->view($list->getTasks(), response::HTTP_OK);
     }
 
 
     /**
      * Actualizar todos los campos del registro
-     * @Rest\Put("/lists", name="put_lists")
+     * @Rest\Patch("/lists/{id}/title", name="patch_lists_title")
+     * @Rest\RequestParam(name="title", description="title update", nullable=false)
+     * @param ParamFetcher $paramFetcher
+     * @param TaskList $list
+     * @return \FOS\RestBundle\View\View
      */
-    public function putListsAction()
+    public function patchTitleListsAction(ParamFetcher $paramFetcher, TaskList $list)
     {
+        // TODO: Parametros provenientes de la petición
+        $title = $paramFetcher->get('title');
 
+        // TODO: Se comprueba si no es null y se procede con la actialización
+        if(!is_null($title)) {
+            $list->setTitle($title);
+            $this->entityManager->flush();
+
+            return $this->view($list, Response::HTTP_CREATED);
+        }
+
+        // TODO: En caso de fallar, devuelve un mensaje
+        return $this->view(['message' => 'Error en el proceso de actualizado'], Response::HTTP_BAD_REQUEST);
     }
 
 
     /**
      * Actualizar un registro (por patch)
-     * @Rest\Patch("/lists/{id}/background", name="background_lists")
+     * @Rest\Patch("/lists/{id}/background", name="patch_lists_background")
      * @Rest\FileParam(name="image", description="La imagen de la lista", nullable=false, image=true)
      * @param Request $request
      * @param ParamFetcher $paramFetcher
-     * @param $id
+     * @param TaskList $list
      * @return \FOS\RestBundle\View\View
      */
-    public function patchBackgroundListsAction(Request $request, ParamFetcher $paramFetcher, $id)
+    public function patchBackgroundListsAction(Request $request, ParamFetcher $paramFetcher, TaskList $list)
     {
-        // TODO: Se obtiene el registro a actualizar
-        $list = $this->taskListRepository->findOneBy(['id' => $id]);
-
         // TODO: Se obtiene la imagen actual
         $backgrundactual = $list->getBackground();
 
         //TODO: Si contiene un elemento se remueve
-        if(!is_null($backgrundactual)) {
+        if (!is_null($backgrundactual)) {
             $filesystem = new Filesystem();
             $filesystem->remove($this->getUploadsDir() . $backgrundactual);
         }
@@ -124,7 +144,7 @@ class ListController extends AbstractFOSRestController
         $file = $paramFetcher->get('image');
 
         // TODO: Si recibe correctamente
-        if($file) {
+        if ($file) {
             // TODO: se obtiene el nombre del archivo
             $filename = md5(uniqid()) . '.' . $file->guessClientExtension();
             // TODO: se mueve a la carpeta asignada
@@ -133,11 +153,10 @@ class ListController extends AbstractFOSRestController
             // TODO: Se actualiza unicamente los campos backgroud y backgroundPath de la entidad
             $list->setBackground($filename);
             $list->setBackgroundPath('/image/uploads/' . $filename);
-
             $this->entityManager->persist($list);
             $this->entityManager->flush();
 
-            // TODO:
+            // TODO: Retorna la url perteneciente al background actualizado
             $data = $request->getUriForPath($list->getBackgroundPath());
             Return $this->view($data, Response::HTTP_OK);
         }
@@ -149,7 +168,26 @@ class ListController extends AbstractFOSRestController
      * Función que retorna la ruta donde se alojan las imagenes
      * que se suben al servidor
      */
-    public function getUploadsDir() {
+    public function getUploadsDir()
+    {
         return $this->getParameter('uploads_dir');
+    }
+
+
+    /**
+     * @Rest\Delete("/lists/{id}", name="delete_lists")
+     * @param TaskList $list
+     * @return \FOS\RestBundle\View\View
+     */
+    public function deleteListAction(TaskList $list)
+    {
+        if(!is_null($list)) {
+            $this->entityManager->remove($list);
+            $this->entityManager->flush();
+
+            return $this->view(['message' => 'El registro se borro exitósamente.'], response::HTTP_OK);
+        }
+
+        return $this->view(['message' => 'Algo salió mal al intetar borrar un registro'], response::HTTP_BAD_REQUEST);
     }
 }
